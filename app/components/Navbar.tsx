7ac4cd96c509c4cd6c11736bb81c1b22022cb1cb
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-19 17:06:44 +0200

    remove some links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,22 +15,10 @@ function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li>
-                            <Link
-                                href="/genre-list"
-                            >
-                                genre
-                            </Link>
-                        </li>
                         <li><Link
                             href="/popular"
                         >
                             popular
-                        </Link></li>
-                        <li><Link
-                            href="/recent"
-                        >
-                            recent
                         </Link></li>
                     </ul>
                 </div>
@@ -40,7 +28,7 @@ function Navbar() {
                 <Link
                     href="/"
                     className="btn btn-ghost normal-case text-xl"  >
-                    Anime Variant
+                    Anime Variant Scans
                 </Link>
             </div>
             <div className="navbar-end">
