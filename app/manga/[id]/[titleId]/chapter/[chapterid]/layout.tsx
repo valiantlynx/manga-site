d15d1efe176aab2716253a2956fa9e686258a476
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-19 17:32:44 +0200

    css

--- app/manga/[id]/[titleId]/chapter/[chapterid]/layout.tsx
+++ app/manga/[id]/[titleId]/chapter/[chapterid]/layout.tsx
@@ -1,4 +1,4 @@
-import '../globals.css'
+import '@/app/globals.css'
 
 export async function generateMetadata({ params }: { params: { id: string, titleId: string, chapterid: string } }) {
     const { id, titleId, chapterid } = params
--- app/manga/[id]/[titleId]/layout.tsx
+++ app/manga/[id]/[titleId]/layout.tsx
@@ -1,4 +1,4 @@
-import '../globals.css'
+import '@/app/globals.css'
 import getDetails from '@/utils/getDetails';
 
 export async function generateMetadata({ params }: { params: { id: string, titleId: string } }) {
