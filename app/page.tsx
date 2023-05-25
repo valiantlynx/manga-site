1b9192fb0ac73869d6d6fba429cc7f038acef87f
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-25 03:56:44 +0200

    gg

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-base-200">
       <Carousel mangaListArray={mangaList} />
-      <PopularManga />
     </main>
   )
 }
