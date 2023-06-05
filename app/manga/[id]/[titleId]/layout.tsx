0e4307228c3ddc7ab52a31089bf004187fa5f02e
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-06-05 16:59:28 +0200

    removed generate metadata from a function in details page

--- app/manga/[id]/[titleId]/layout.tsx
+++ app/manga/[id]/[titleId]/layout.tsx
@@ -1,19 +1,10 @@
 import '@/app/globals.css'
-import getDetails from '@/utils/getDetails';
 
-export async function generateMetadata({ params }: { params: { id: string, titleId: string } }) {
+export function generateMetadata({ params }: { params: { id: string, titleId: string } }) {
     const { id, titleId } = params
-    const data: any = await getDetails(id, titleId); // deduped
-    console.log(" - generating metadata for " + titleId, id + " - ", data)
 
-    if (!data) {
-      return {
-        title: 'Post Not Found',
-      }
-    }
-  
     return {
-      data,
+      title: titleId,
     }
   }
 
