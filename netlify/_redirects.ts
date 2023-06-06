1cae0a7efe7433ac2668cc55e352176364ddb044
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-06-06 12:53:16 +0200

    i give up

--- netlify/_redirects.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { url = '/' } = req.query;
-  res.writeHead(301, {
-    Location: `https://animevariant.org${url}`,
-  });
-  res.end();
-}
