e21965dbf22f7ea6f50ba3bd2f2318fbb135311b
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-10-24 23:41:41 +0200

    svelte-blog

---
+++ apps/svelte-blog/setup.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=apps/urara https://github.com/valiantlynx/urara.git valiantlynx-turborepo --squash
+git subtree pull --prefix=apps/urara https://github.com/valiantlynx/urara.git valiantlynx-turborepo --squash
+git subtree push --prefix=apps/urara https://github.com/valiantlynx/urara.git valiantlynx-turborepo
