cd75b8f05aecef14778a6605154ccb050586daba
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-10-25 13:59:47 +0200

    ss

--- apps/svelte-blog/src/lib/utils/stores.ts
+++ apps/svelte-blog/src/lib/utils/stores.ts
@@ -1,5 +1,4 @@
 // stores.js
-<<<<<<< HEAD
 import { writable } from 'svelte/store';
 
 // Initial cart state
@@ -81,46 +80,3 @@ export const stationImageMap = {
 
 // Current page
 export const minimum_fuel_economy = 0.1;
-=======
-import { writable } from 'svelte/store'
-
-// Initial cart state
-const initialCart: any[] = []
-
-// Initial cart state
-const initialUser: any = {}
-
-// Create the cart store
-export const cart = writable(initialCart)
-export const order = writable(initialCart)
-
-// Current User
-export const authData = writable(initialUser)
-
-const sidebarOpen = writable(true)
-
-const toggleSidebar = () => {
-  sidebarOpen.update(prev => !prev)
-}
-
-const closeSidebar = () => {
-  sidebarOpen.update(() => false)
-}
-
-export { sidebarOpen, toggleSidebar, closeSidebar }
-
-// Current sidebar section
-export const currentSection = writable('')
-
-// Current page
-export const page = writable(1)
-
-// pocketbase response
-export const pocketbaseResponse = writable({
-  page: 1,
-  perPage: 5,
-  totalItems: 0,
-  totalPages: 0,
-  items: []
-})
->>>>>>> 6fd27d3a271e15ef994600dd978743c1f6389fd2
