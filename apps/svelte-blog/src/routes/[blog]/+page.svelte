badce5d6bd50ce313d76b6654e0de4c1e8404d7b
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-01 03:30:04 +0100

    rename

--- apps/svelte-blog/src/routes/[blog]/+page.svelte
+++ apps/svelte-blog/src/routes/[blog]/+page.svelte
@@ -1,18 +1,22 @@
 <script>
 	import { page } from '$app/stores';
+  const blog = $page.data.blog;
 </script>
 
 <div class="p-4 md:p-8 lg:p-12 flex justify-center">
 	<div class="w-3/4">
 		<h1 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
-			{$page.data.blog?.title}
+			{blog?.title}
 		</h1>
 		<img
-			src={$page.data.blog?.image}
-			alt={$page.data.blog?.title}
+			src={blog?.image}
+			alt={blog?.title}
 			class="w-full h-auto rounded-lg mb-4"
 		/>
-		<p class="prose mb-4">{@html $page.data.blog?.content}</p>
-		<p class="text-sm text-secondary">Tags: {$page.data.blog?.tags}</p>
+
+      {@html blog?.content}
+
+		<p class="text-sm text-secondary">Tags: {blog?.tags}</p>
 	</div>
 </div>
+
