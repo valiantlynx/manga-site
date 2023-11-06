ab8c6ab5ec08c17086efce7bc901941f0bd2c40a
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-06 04:07:46 +0100

    back

--- apps/chatbotAI/setup.sh
+++ apps/chatbotAI/setup.sh
@@ -4,10 +4,11 @@ docker exec chatbotai-main /bin/bash
 
 # make a brach on the main repo named the same as the monorepo
 # add this as a subtree to the main repo
-git subtree add --prefix=packages/svelte-rich-text https://github.com/valiantlynx/svelte-rich-text.git valiantlynx-turborepo --squash
+git subtree add --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git valiantlynx-turborepo --squash
 
 # pull the subtree
-git subtree pull --prefix=packages/svelte-rich-text https://github.com/valiantlynx/svelte-rich-text.git valiantlynx-turborepo --squash
+git subtree pull --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git valiantlynx-turborepo --squash
 
 # push the subtree
-git subtree push --prefix=packages/svelte-rich-text https://github.com/valiantlynx/svelte-rich-text.git valiantlynx-turborepo
+git subtree push --prefix=apps/chatbotAI https://github.com/valiantlynx/chatbotAI.git valiantlynx-turborepo
+
