a1628472fe61a6da2c250298064ceb12b06779da
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-10 02:28:57 +0100

    can have the svelte-rich-text

--- apps/svelte-blog/src/routes/[blog]/+page.svelte
+++ apps/svelte-blog/src/routes/[blog]/+page.svelte
@@ -1,12 +1,17 @@
 <script>
 	import { page } from '$app/stores';
+	import { ValiantRichText } from '@valiantlynx/svelte-rich-text';
 
   const blog = $page.data.blog;
 </script>
 
 
+
 <div class="p-4 md:p-8 lg:p-12 flex justify-center">
+
+
 	<div class="w-3/4">
+		
 		<h1 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
 			{blog?.title}
 		</h1>
@@ -15,7 +20,7 @@
 			alt={blog?.title}
 			class="w-full h-auto rounded-lg mb-4"
 		/>
-
+		<ValiantRichText />
     <p class="text-sm text-accent">By: {blog?.author}</p>
 
     <p class="text-sm text-accent">Published: {blog?.created}</p>
