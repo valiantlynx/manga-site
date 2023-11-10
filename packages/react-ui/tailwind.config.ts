c362c7cce3788a75ca2ca24fdbcef5d4ff98efae
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-10 17:43:38 +0100

    react ui

--- packages/react-ui/tailwind.config.ts
+++ packages/react-ui/tailwind.config.ts
@@ -1,6 +1,6 @@
 // tailwind config is required for editor support
 import type { Config } from "tailwindcss";
-import sharedConfig from "tailwind-config/tailwind.config";
+import sharedConfig from "@valiantlynx/tailwind-config/tailwind.config";
 
 const config: Pick<Config, "prefix" | "presets"> = {
   prefix: "ui-",
