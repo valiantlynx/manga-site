fd4501fd545a80c52b9d5eaa22866b46462e7377
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-11 01:40:22 +0100

    gg

--- packages/tsconfig/run.sh
+++ packages/tsconfig/run.sh
@@ -1,4 +1,4 @@
 
-git subtree pull --prefix=packages/tsconfig https://github.com/valiantlynx/tsconfig.git main
+git subtree pull --prefix=packages/tsconfig https://github.com/valiantlynx/tsconfig.git valiantlynx-turborepo
 
-git subtree push --prefix=packages/tsconfig https://github.com/valiantlynx/tsconfig.git main
+git subtree push --prefix=packages/tsconfig https://github.com/valiantlynx/tsconfig.git valiantlynx-turborepo
