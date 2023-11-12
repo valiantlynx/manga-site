c8bfcba9720d29b750e081542bf3d40634ff22f3
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-12 06:21:22 +0100

    Squashed 'apps/svelte-ui/' content from commit 9e4c70a
    git-subtree-dir: apps/svelte-ui
    git-subtree-split: 9e4c70ae7f6145c67f494adc555eb386ca390ca6

---
+++ src/lib/components/SideBarIcon.svelte
@@ -0,0 +1,20 @@
+<script>
+	// @ts-nocheck
+
+	import Icon from '@iconify/svelte';
+	/**
+	 * @type {any}
+	 */
+	export let tooltip;
+	/**
+	 * @type {any}
+	 */
+	export let icon;
+</script>
+
+<div class="sidebar-icon group">
+	<Icon {icon} />
+	<span class="sidebar-tooltip group-hover:scale-100">
+		{tooltip}
+	</span>
+</div>
