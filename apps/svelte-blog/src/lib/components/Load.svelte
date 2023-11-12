f270797f7c36fad4c9b805eca69e3aa59b281906
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-12 06:21:15 +0100

    load

--- apps/svelte-blog/src/lib/components/Load.svelte
+++ apps/svelte-blog/src/lib/components/Load.svelte
@@ -1,30 +1,13 @@
 <script>
-	import { _ } from 'svelte-i18n';
-
 	export let loaded;
 </script>
 
 {#if !loaded}
-	<!-- <div class="flex items-center justify-center h-17">
-		<div class="text-center">
-			<span class="mdi mdi-loading mdi-spin text-8xl" />
-			<p class="mt-4 text-4xl">{$_('page.load.retrieving')}</p>
-		</div>
-
-		<div class="p-2">
-			<img
-				src="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
-				alt="Receipt"
-				width="100"
-				class="mt-4 max-w-20"
-			/>
-		</div>
-	</div> -->
 	<div
 		class="absolute bg-primary bg-opacity-60 text-primary-content z-10 h-full w-full flex items-center justify-center"
 	>
 		<div class="flex items-center">
-			<span class="text-3xl mr-4">{$_('page.load.retrieving')}</span>
+			<span class="text-3xl mr-4">retrieving data ...</span>
 			<!-- loading icon -->
 			<svg
 				class="animate-spin h-5 w-5"
