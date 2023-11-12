2a20e282c0752251580ee552a0f814be25e00715
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-12 13:05:40 +0100

    "autoprefixer": "^10.4.16", 		"postcss": "^8.4.31", 		"tailwindcss": "^3.3.5"

--- packages/svelte-ui/src/lib/components/SideBarIcon.svelte
+++ packages/svelte-ui/src/lib/components/SideBarIcon.svelte
@@ -12,9 +12,9 @@
 	export let icon;
 </script>
 
-<div class="sidebar-icon group">
+<div class="relative flex items-center justify-center w-12 h-12 m-2 mb-2 mx-auto shadow-lg bg-base-100 text-base-content hover:bg-secondary hover:text-primary-content rounded-3xl hover:rounded-xl transition-all duration-300 ease-linear cursor-pointer group">
 	<Icon {icon} />
-	<span class="sidebar-tooltip group-hover:scale-100">
+	<span class="absolute w-auto z-10 p-2 m-2 min-w-max left-14 rounded-md shadow-md text-base-content bg-base-200 text-sm font-bold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
 		{tooltip}
 	</span>
 </div>
