02d201575990afdd9cb3597a132e1aa7dfa69672
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-14 17:36:35 +0100

    Squashed 'apps/react-manga-translator/' content from commit 5416b42
    git-subtree-dir: apps/react-manga-translator
    git-subtree-split: 5416b427df878708b65fa66b8008d9b2ef57f499

---
+++ run.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=apps/react-manga-translator https://github.com/valiantlynx/react-manga-translator.git valiantlynx-turborepo --squash
+git subtree pull --prefix=apps/react-manga-translator https://github.com/valiantlynx/react-manga-translator.git valiantlynx-turborepo --squash
+git subtree push --prefix=apps/react-manga-translator https://github.com/valiantlynx/react-manga-translator.git valiantlynx-turborepo
