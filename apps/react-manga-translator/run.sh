d9c9c74eea41f980a480eb07e28464294ec766be
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-14 20:18:31 +0100

    emove rwact manga translater

--- apps/react-manga-translator/run.sh
+++
@@ -1,3 +0,0 @@
-git subtree add --prefix=apps/react-manga-translator https://github.com/valiantlynx/react-manga-translator.git valiantlynx-turborepo --squash
-git subtree pull --prefix=apps/react-manga-translator https://github.com/valiantlynx/react-manga-translator.git valiantlynx-turborepo --squash
-git subtree push --prefix=apps/react-manga-translator https://github.com/valiantlynx/react-manga-translator.git valiantlynx-turborepo
