d8058cdb229b90700c47d8d96a4331274a26919d
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-19 06:49:57 +0100

    logos

--- apps/svelte-manga/src/lib/config/site.ts
+++ apps/svelte-manga/src/lib/config/site.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from '$lib/types/site';
 export const site: SiteConfig = {
 	protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
 	domain: import.meta.env.VITE_SITE_URL ?? 'animevariant.com',
-	logo: import.meta.env.VITE_SITE_LOGO ?? '/logo.png',
+	logo: import.meta.env.VITE_SITE_LOGO ?? '/logo.svg',
 	company: import.meta.env.VITE_SITE_COMPANY ?? 'Valiantlynx',
 	email: import.meta.env.VITE_SITE_EMAIL ?? 'valiantlynxz@gmail.com',
 	noImage: import.meta.env.VITE_SITE_NO_IMAGE ?? '/assets/no-image.png',
