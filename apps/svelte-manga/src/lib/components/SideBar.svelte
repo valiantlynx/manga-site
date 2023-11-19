ba731ac5598fc84aa4e46f39fe8a7d601a3906aa
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-19 08:19:20 +0100

    login works

--- apps/svelte-manga/src/lib/components/SideBar.svelte
+++ apps/svelte-manga/src/lib/components/SideBar.svelte
@@ -1,17 +1,27 @@
 <script>
-	import SideBarIcon from './SideBarIcon.svelte';
-	import { logoutPocketbase } from '$lib/utils/api';
+	import SideBarIcon  from '$lib/components/SideBarIcon.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <div
-	class="relative top-0 left-0 px-2 w-16 h-screen bg-base-200 m-0 flex flex-col text-base-content shadow"
+	class="relative top-0 left-0 w-16 h-screen bg-base-200 m-0 flex flex-col text-base-content shadow"
 >
-	<a href="/"><SideBarIcon tooltip="Home" icon="bx:bx-home" /></a>
 	<a href="/dashboard/profile"> <SideBarIcon tooltip="Profile" icon="iconoir:profile-circle" /></a>
 	<a href="/dashboard/reading-progress"
 		><SideBarIcon tooltip="reading progress" icon="fluent-mdl2:reading-mode" /></a
 	>
 	<a href="/dashboard/settings"><SideBarIcon tooltip="Settings" icon="bx:bx-cog" /></a>
+	{#if $page.data.user?.role.includes('admin')}
+		<a href="/dashboard/admin"><SideBarIcon tooltip="Admin" icon="bx:bx-shield" /></a>
+		<a href="/dashboard/upload"><SideBarIcon tooltip="Upload" icon="ph:upload-bold" /></a>
+	{/if}
+	{#if $page.data.user?.role.includes('creator')}
 	<a href="/dashboard/upload"><SideBarIcon tooltip="Upload" icon="ph:upload-bold" /></a>
-	<button on:click={logoutPocketbase}><SideBarIcon tooltip="Logout" icon="bx:bx-log-out" /></button>
+	{/if}
+
+	<form action="/api/logout" method="POST" class="absolute bottom-1/4 w-full">
+		<button type="submit" class="w-full">
+			<SideBarIcon tooltip="Logout" icon="bx:bx-log-out" />
+		</button>
+	</form>
 </div>
