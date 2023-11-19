a4ef1a5a3353bfeaecabfe08a672726167bcccf4
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-19 07:47:48 +0100

    moved alot of stuff, upgraded svelte-manga

--- apps/svelte-manga/src/routes/dashboard/profile/edit/+page.svelte
+++ apps/svelte-manga/src/routes/dashboard/profile/edit/+page.svelte
@@ -1,106 +1,141 @@
-<script lang="ts">
-	import InputWithLabel from '$lib/components/InputWithLabel.svelte';
-	import { authData } from '$lib/utils/stores';
-	import { compressFileImage, patchPocketbase } from '$lib/utils/api';
+<script>
+		import {Input} from '@valiantlynx/svelte-ui';
+	import { enhance, applyAction } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { site } from '@valiantlynx/general-config';
 
-	let updated: boolean;
+	let loading;
 
-	let data: any = {
-		username: $authData.username,
-		title: $authData.title,
-		address: $authData.address,
-		about: $authData.about,
-		language: $authData.language,
-		timezone: $authData.timezone,
-		avatar: $authData.avatar
+	$: loading = false;
+	const showPreview = (event) => {
+		const target = event.target;
+		const files = target.files; //await compressFileImage(data.avatar, 200, 200, 0.7);
+
+		if (files.length > 0) {
+			const src = URL.createObjectURL(files[0]);
+			const preview = document.getElementById('avatar-preview');
+			preview.src = src;
+		}
 	};
 
-	async function updateProfile() {
-		// console.log('updateProfile from', $authData, 'to', data);
-		try {
-			// Convert image to compressed base64 before uploading
-			const compressedImage = await compressFileImage(data.avatar, 200, 200, 0.7);
-			data.avatar = compressedImage;
-
-			const formData = new FormData();
-			formData.append('data', JSON.stringify(data)); // Include the entire data object
-			for (const key in data) {
-				formData.append(key, data[key]);
+	const submitUpdateProfile = () => {
+		loading = true;
+		return async ({ result }) => {
+			switch (result.type) {
+				case 'success':
+					await invalidateAll();
+					break;
+				case 'error':
+					break;
+				default:
+					await applyAction(result);
 			}
+			loading = false;
+		};
+	};
 
-			// Exclude email and verified fields from being updated
-			await patchPocketbase('users', $authData.id, formData);
-
-			// Update authData store
-			//refreshAuthPocketbase()
-
-			// if (window){
-			// 	window.location.reload()
-			// }
-
-			updated = true;
-		} catch (error) {
-			throw new Error('Failed to patch user.');
-		}
-	}
-
-	function handleFiles(event: any) {
-		data.avatar = event.target.files[0];
-	}
+	const avatar = $page.data.user?.avatar
+		? `${site.site.pocketbase}/api/files/${$page.data.user?.collectionId}/${$page.data.user?.id}/${$page.data.user?.avatar}`
+		: `https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=${$page.data.user?.username}`;
 </script>
 
 <svelte:head>
 	<title>Edit</title>
 </svelte:head>
+<div class="flex flex-col w-full h-full p-4">
+	<form
+		action="?/updateProfile"
+		method="POST"
+		class="flex flex-col space-y-4 w-full max-w-screen-xl mx-auto md:flex-row md:space-x-8"
+		enctype="multipart/form-data"
+		use:enhance={submitUpdateProfile}
+	>
+		<div class="md:w-1/2">
+			<div class="form-control">
+				<label for="avatar" class="label font-medium">
+					<span class="label-text">Profile Picture</span>
+				</label>
+				<label for="avatar" class="avatar w-32 rounded-full hover:cursor-pointer relative">
+					<span
+						class="btn btn-circle btn-sm btn-secondary absolute -bottom-2 -right-2 hover:cursor-pointer"
+					>
+						<i class="fa fa-pencil-alt" />
+					</span>
+					<div class="w-32 h-32 rounded-full overflow-hidden">
+						<img src={avatar} alt="user avatar" id="avatar-preview" />
+					</div>
+				</label>
+				<input
+					type="file"
+					name="avatar"
+					id="avatar"
+					value=""
+					accept="image/*"
+					hidden
+					on:change={showPreview}
+					disabled={loading}
+				/>
+			</div>
+		</div>
 
-<p>Edit</p>
+		<div class="md:w-1/2">
+			<h3 class="text-2xl font-medium">Update Profile</h3>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-	<InputWithLabel label="username" bind:value={data.username} />
-	<div class="input input-bordered flex-grow">
-		{$authData.email}
-		<a href="/dashboard/profile/request-email-change" class="btn btn-secondary ml-2 float-right"
-			>Request Email Change
-		</a>
-	</div>
-	<InputWithLabel label="title" bind:value={data.title} />
-	<InputWithLabel label="address" bind:value={data.address} />
+			<Input
+				id="username"
+				label="Username"
+				value={$page.data?.user?.username}
+				disabled={loading}
+			/>
 
-	<textarea
-		placeholder="Type a message..."
-		bind:value={data.about}
-		maxlength="100"
-		class="input input-bordered flex-grow h-28"
-	/>
+			<Input id="title" label="Title" value={$page.data?.user?.title} disabled={loading} />
 
-	<div class="form-control w-full max-w-xs">
-		<label class="label" for="avatar-input">
-			<span class="label-text">Avatar</span>
-		</label>
-		<input
-			type="file"
-			class="file-input file-input-secondary w-full max-w-xs"
-			id="avatar-input"
-			on:change={handleFiles}
-		/>
-	</div>
-</div>
-<div class="divider" />
+			<Input
+				id="language"
+				label="Language"
+				value={$page.data?.user?.language}
+				disabled={loading}
+			/>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-	<InputWithLabel label="language" bind:value={data.language} />
-	<InputWithLabel label="timezone" bind:value={data.timezone} />
-	<div class="flex items-center">
-		{#if $authData.verified}
-			<span class="ml-2 text-success">Verified</span>
-		{:else}
-			<span class="ml-2 text-error">Not Verified</span>
-		{/if}
-	</div>
-</div>
+			<Input
+				type="address"
+				id="address"
+				label="Address"
+				value={$page.data?.user?.address}
+				disabled={loading}
+			/>
 
-<div class="mt-16">
-	<button class="btn btn-primary float-right" disabled={updated} on:click={updateProfile}>
-		{!updated ? 'Update' : 'sent'}
-	</button>
+			
+		</div>
+
+		<div class="w-full">
+			<label for='about' class="label font-medium pb-1">
+				<span class="label-text">About yourself</span>
+			</label>
+			<textarea
+				class="textarea textarea-primary h-32"
+				name="about"
+				id="about"
+				placeholder="Drop a few words about yourself"
+				value={$page.data?.user?.about}
+				disabled={loading}
+				type="text"
+			/>
+			<div class="border-t border-primary mt-4"> 
+				<label for="roles" class="text-xl font-medium">Roles and Permissions</label>
+				<div class="flex flex-wrap -m-1">
+					{#each $page.data?.user?.role as role}
+						<div class="badge badge-primary my-4">{role}</div>
+					{/each}
+				</div>
+			</div>
+		</div>
+
+		<div class="w-full max-w-lg">
+			<button class="btn btn-primary w-full" type="submit" disabled={loading}>
+				Update Profile
+			</button>
+		</div>
+	</form>
 </div>
