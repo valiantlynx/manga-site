71ba4e9ca081a479bc09023d3d3fb040c4537f18
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-22 07:47:06 +0100

    {$page.url.origin}

--- apps/svelte-manga/src/lib/components/ResponsiveBannerAd.svelte
+++ apps/svelte-manga/src/lib/components/ResponsiveBannerAd.svelte
@@ -1,14 +1,17 @@
+<script>
+import { page } from '$app/stores';
+</script>
+
 <div class="w-11/12 py-2 mx-4 rounded-3xl">
 	<script
 		async
-		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7827487944601896"
-		crossorigin="anonymous">{}</script
-	>
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client={$page.data.sites.google_ads_client}"
+		crossorigin="anonymous">{}</script>
 	<!-- animevariant homepage add -->
 	<ins
 		class="adsbygoogle"
 		style="display:block"
-		data-ad-client="ca-pub-7827487944601896"
+		data-ad-client="{$page.data.sites.google_ads_client}"
 		data-ad-slot="3968848442"
 		data-ad-format="auto"
 		data-full-width-responsive="true"
--- apps/svelte-manga/src/routes/manga/+page.svelte
+++ apps/svelte-manga/src/routes/manga/+page.svelte
@@ -5,6 +5,7 @@
 	import ReadingProgress from '$lib/components/ReadingProgress.svelte';
 	import ResponsiveBannerAd from '$lib/components/ResponsiveBannerAd.svelte';
 	import Feedback from '$lib/components/feedback/Feedback.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <h1 class="text-2xl font-bold text-center mb-6">Home</h1>
@@ -36,66 +37,66 @@
 <Feedback />
 
 <svelte:head>
-	<title>AnimeVariant manga page</title>
+	<title>{$page.url.hostname} manga page</title>
 	<!-- Open Graph Meta Tags (for social media sharing) -->
-	<meta property="og:title" content="AnimeVariant" />
+	<meta property="og:title" content="{$page.url.hostname}" />
 	<meta
 		property="og:description"
-		content="Read the latest manga online for free at animevariant.org, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
 	/>
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://animevariant.com/" />
+	<meta property="og:url" content={$page.url.origin} />
 	<meta property="og:image" content="/twitter-image.png" />
-	<meta property="og:image:alt" content="AnimeVariant Logo" />
-	<meta property="og:site_name" content="AnimeVariant" />
+	<meta property="og:image:alt" content="{$page.url.hostname} Logo" />
+	<meta property="og:site_name" content="{$page.url.hostname}" />
 
 	<!-- Twitter Meta Tags (for social media sharing) -->
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="AnimeVariant" />
+	<meta name="twitter:title" content="{$page.url.hostname}" />
 	<meta
 		name="twitter:description"
-		content="Read the latest manga online for free at animevariant.org, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
 	/>
 	<meta name="twitter:image" content="/twitter-image.png" />
 
 	<!-- Google / Search Engine Tags -->
-	<meta itemprop="name" content="AnimeVariant" />
+	<meta itemprop="name" content="{$page.url.hostname}" />
 	<meta
 		itemprop="description"
-		content="Read the latest manga online for free at animevariant.org, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
 	/>
 	<meta itemprop="image" content="/twitter-image.png" />
 
 	<!-- Facebook Meta Tags (for social media sharing) -->
 	<meta property="fb:app_id" content="AnimeVariant" />
 	<meta property="fb:admins" content="AnimeVariant" />
-	<meta property="fb:page_id" content="AnimeVariant" />
-	<meta property="fb:site_name" content="AnimeVariant" />
-	<meta property="article:publisher" content="AnimeVariant" />
-	<meta property="article:author" content="AnimeVariant" />
-	<meta property="article:section" content="AnimeVariant" />
-	<meta property="article:tag" content="AnimeVariant" />
-	<meta property="article:published_time" content="AnimeVariant" />
-	<meta property="article:modified_time" content="AnimeVariant" />
-	<meta property="article:expiration_time" content="AnimeVariant" />
-	<meta property="article:author:first_name" content="AnimeVariant" />
-	<meta property="article:author:last_name" content="AnimeVariant" />
-	<meta property="article:author:username" content="AnimeVariant" />
+	<meta property="fb:page_id" content="{$page.url.hostname}" />
+	<meta property="fb:site_name" content="{$page.url.hostname}" />
+	<meta property="article:publisher" content="{$page.url.hostname}" />
+	<meta property="article:author" content="{$page.url.hostname}" />
+	<meta property="article:section" content="{$page.url.hostname}" />
+	<meta property="article:tag" content="{$page.url.hostname}" />
+	<meta property="article:published_time" content="{$page.url.hostname}" />
+	<meta property="article:modified_time" content="{$page.url.hostname}" />
+	<meta property="article:expiration_time" content="{$page.url.hostname}" />
+	<meta property="article:author:first_name" content="{$page.url.hostname}" />
+	<meta property="article:author:last_name" content="{$page.url.hostname}" />
+	<meta property="article:author:username" content="{$page.url.hostname}" />
 
 	<!-- Schema.org Meta Tags (for SEO) -->
-	<meta itemprop="headline" content="AnimeVariant" />
+	<meta itemprop="headline" content="{$page.url.hostname}" />
 	<meta
 		itemprop="description"
-		content="Read the latest manga online for free at animevariant.org, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
 	/>
 	<meta itemprop="image" content="/twitter-image.png" />
 	<!-- Keywords Meta Tag -->
 	<meta
 		name="keywords"
-		content="manga, read manga, read manga online, request manga, manga reading website, populate manga, manga online, manga, manga details, manga chapters, read manga free, manga free, read free manga, read free manga online, manga reader, manga viewer, manga scans, animevariant, AnimeVariant.org "
+		content="manga, read manga, read manga online, request manga, manga reading website, populate manga, manga online, manga, manga details, manga chapters, read manga free, manga free, read free manga, read free manga online, manga reader, manga viewer, manga scans, {$page.url.hostname}, {$page.url.origin} "
 	/>
 	<meta
 		name="description"
-		content="Read the latest manga online for free at animevariant.org, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
+		content="Read the latest manga online for free at {$page.url.origin}, update fastest, most full, synthesized 24h free with high-quality images and be the first one to publish new chapters."
 	/>
 </svelte:head>
