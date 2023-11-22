0cb217ebb67f69e3043bbf7112ba48352061f780
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-22 08:15:23 +0100

    $page.data.siteName

--- apps/svelte-manga/src/lib/components/Nav.svelte
+++ apps/svelte-manga/src/lib/components/Nav.svelte
@@ -9,7 +9,7 @@
 <div>
 	<div class="navbar bg-base-300 max-w-full sm:hidden">
 		<div class="flex-1">
-			<a class="btn btn-ghost normal-case text-xl" href="/">{$page.url.host}</a>
+			<a class="btn btn-ghost normal-case text-xl" href="/">{$page.data.siteName}</a>
 			<ThemeChanger />
 		</div>
 		<ProfileModal />
