66090e6211325bec8ddb03fe65c91ffcaf3cf099
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-11-28 00:55:36 +0100

    working terraform docker

--- apps/svelte-manga/main.tf
+++ apps/svelte-manga/main.tf
@@ -9,6 +9,21 @@ terraform {
 
 provider "docker" {}
 
+resource "docker_image" "svelte_manga" {
+  name         = "valiantlynx/svelte-manga" # Adjust this to match your image name
+  keep_locally = true
+}
+
+resource "docker_container" "svelte_manga" {
+  image = docker_image.svelte_manga.image_id
+  name  = "svelte-manga"
+
+  ports {
+    internal = 3000
+    external = 3000 # You might want to change this depending on your setup
+  }
+}
+
 resource "docker_image" "nginx" {
   name         = "nginx"
   keep_locally = false
@@ -16,10 +31,10 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = "nginx-proxy"
 
   ports {
     internal = 80
-    external = 8000
+    external = 80 # Expose nginx on port 80
   }
 }
