6b60550653c191deb142786d2d3b0530be4306d3
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-04 03:19:10 +0100

    nicer images

--- apps/svelte-manga/src/routes/api/getimage/+server.ts
+++ apps/svelte-manga/src/routes/api/getimage/+server.ts
@@ -2,44 +2,50 @@ import sharp from 'sharp';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
-	const imageUrl: any = url.searchParams.get('url');
-	const quality = Number(url.searchParams.get('quality')) || 100;
+    const imageUrl: any = url.searchParams.get('url');
+    const upscaleFactor = 2; // Upscale by a factor of 2
 
-	console.log('imageUrl', imageUrl);
+    console.log('imageUrl', imageUrl);
 
-	try {
-		// Fetch the image
-		const response = await fetch(imageUrl, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'image/jpeg',
-				'Access-Control-Allow-Origin': '*'
-			}
-		});
+    try {
+        // Fetch the image
+        const response = await fetch(imageUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'image/jpeg',
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
 
-		if (!response.ok) {
-			throw new Error('Failed to fetch image');
-		}
+        if (!response.ok) {
+            throw new Error(`Failed to fetch image: ${response.statusText}`);
+        }
 
-		// Read the image data as ArrayBuffer
-		const imageArrayBuffer = await response.arrayBuffer();
+        // Read the image data as ArrayBuffer
+        const imageArrayBuffer = await response.arrayBuffer();
 
-		// Process the image using sharp
-		const compressedImageBuffer = await sharp(imageArrayBuffer)
-			.rotate()
-			.webp({ quality })
-			.toBuffer();
+        // Process the image using sharp
+        const enhancedImageBuffer = await sharp(imageArrayBuffer)
+            .rotate() // Optional, based on the image's requirements
+            .sharpen() // Sharpen the image
+            .modulate({ // Adjust brightness and contrast
+                brightness: 1.1,
+                contrast: 1.1
+            })
+            .webp({ quality: 100 }) // Convert to WebP with highest quality
+            .toBuffer();
 
-		return new Response(compressedImageBuffer, {
-			headers: {
-				'Content-Type': 'image/webp',
-				'Access-Control-Allow-Origin': '*',
-				'Cache-Control': `public, s-maxage=${60 * 60 * 24 * 365}`
-			}
-		});
-	} catch (error) {
-		return new Response(JSON.stringify({ message: 'Failed to compress image', error }), {
-			status: 500
-		});
-	}
+        return new Response(enhancedImageBuffer, {
+            headers: {
+                'Content-Type': 'image/webp',
+                'Access-Control-Allow-Origin': '*',
+                'Cache-Control': `public, s-maxage=${60 * 60 * 24 * 365}`
+            }
+        });
+    } catch (error) {
+        console.error('Error processing image:', error);
+        return new Response(JSON.stringify({ message: 'Failed to process image', error }), {
+            status: 500
+        });
+    }
 };
