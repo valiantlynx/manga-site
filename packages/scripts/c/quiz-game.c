fec7616d1ad018d0ffbfacc1ded66d65cd777fa4
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-08 22:29:09 +0100

    logseq online baby

---
+++ packages/scripts/c/quiz-game.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char questions[][100] = {
+        "What is the capital of India?\n",
+        "What is the capital of USA?\n",
+        "What is the capital of Russia?\n",
+        "What is the capital of China?\n",
+        "What is the capital of Japan?\n",
+        "What is the capital of Australia?\n",
+        "What is the capital of Canada?\n",
+        "What is the capital of Brazil?\n",
+        "What is the capital of Argentina?\n",
+        "What is the capital of South Africa?\n"};
+
+    char options[][100] = {
+        "A. New Delhi\nB. Mumbai\nC. Kolkata\nD. Chennai",
+        "A. New York\nB. Washington DC\nC. Los Angeles\nD. Chicago",
+        "A. Moscow\nB. St. Petersburg\nC. Novosibirsk\nD. Yekaterinburg",
+        "A. Beijing\nB. Shanghai\nC. Tianjin\nD. Guangzhou",
+        "A. Tokyo\nB. Osaka\nC. Kyoto\nD. Yokohama",
+        "A. Sydney\nB. Melbourne\nC. Brisbane\nD. Perth",
+        "A. Toronto\nB. Montreal\nC. Vancouver\nD. Ottawa",
+        "A. Sao Paulo\nB. Rio de Janeiro\nC. Brasilia\nD. Salvador",
+        "A. Buenos Aires\nB. Cordoba\nC. Rosario\nD. Mendoza",
+        "A. Johannesburg\nB. Cape Town\nC. Durban\nD. Pretoria"};
+
+    char answers[10] = {'A', 'B', 'A', 'A', 'A', 'A', 'D', 'C', 'A', 'B'};
+    int numberOfQuestions = sizeof(questions) / sizeof(questions[0]);
+
+    char guess;
+    char score;
+
+    printf("QUIZ GAME!\n");
+
+    for (int i = 0; i < numberOfQuestions; i++) {
+        printf("%s", questions[i]);
+    };
+
+    return 0;
+}
