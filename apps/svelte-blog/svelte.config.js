b358e37ed0c3c7624c72bb5e0b5e9bd620f56fdd
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-09 09:04:45 +0100

    docker better

--- apps/svelte-blog/svelte.config.js
+++ apps/svelte-blog/svelte.config.js
@@ -1,6 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const deployTarget = process.env.DEPLOY_TARGET || 'auto'; // Default to 'auto'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,7 +15,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: deployTarget === 'auto' ? adapterAuto() : adapterNode()
 	}
 };
 
