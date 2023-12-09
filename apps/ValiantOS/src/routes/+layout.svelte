5eb1fb322fda06a888492c874189b9c7f7e823b3
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-09 20:58:58 +0100

    service worker

--- apps/ValiantOS/src/routes/+layout.svelte
+++ apps/ValiantOS/src/routes/+layout.svelte
@@ -1,6 +1,27 @@
 <script>
     import '$lib/styles/app.css'
     import '$lib/styles/tailwind.css'
+  import { onMount } from 'svelte';
+
+    async function detectServiceWorkerUpdate() {
+        const registration = await navigator.serviceWorker.ready
+
+        registration.addEventListener('updatefound', () => {
+            const newServiceWorker = registration.installing
+            newServiceWorker?.addEventListener('statechange', () => {
+                if (newServiceWorker.state == 'installed') {
+                    if (confirm('New update Available! Reload to update')) {
+                        newServiceWorker.postMessage({type: 'SKIP_WAITING'})
+                        window.location.reload
+                    }
+                }
+            })
+        })
+    }
+
+    onMount(() => {
+        detectServiceWorkerUpdate()
+    });
 </script>
 
 <slot/>
